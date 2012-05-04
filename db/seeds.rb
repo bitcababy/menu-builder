bc29ca5fb1172efab005495bee0d870e8f9b6ba2
Meredith Lesly <bitcababy+github@gmail.com>
2012-05-04 12:08:41 -0400

    Created minimal menubar setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Menu.delete_all
+
+menubar = Menubar.create!
+
+menubar.children << (Menu.create! text: 'Home')
+menubar.children << (mp = Menu.create! text: 'Products')
+menubar.save!
+
+mp.children << (Menu.create! text: 'Product 1')
+mp.children << (Menu.create! text: 'Product 2')
+mp.save!
+
+
+
+

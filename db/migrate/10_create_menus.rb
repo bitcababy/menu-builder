eff42dd2758b69b198cab827e713c7a9e35c7f21
Meredith Lesly <bitcababy+github@gmail.com>
2012-05-04 12:20:09 -0400

    Create menus table with some of the necessary attributes

---
+++ db/migrate/10_create_menus.rb
@@ -0,0 +1,17 @@
+class CreateMenus < ActiveRecord::Migration
+  def change
+    create_table :menus do |t|
+			t.string		:type
+      t.integer 	:parent_id
+      t.string 		:text
+      t.boolean 	:enabled
+			t.boolean		:visible
+			t.string		:link
+
+      t.timestamps
+    end
+
+		add_index :menus, :type
+		add_index :menus, :parent_id
+  end
+end

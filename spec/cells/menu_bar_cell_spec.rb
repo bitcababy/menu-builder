9da55f2d93cd7eefa86efe438674f2cd87116496
Meredith Lesly <bitcababy+github@gmail.com>
2012-05-04 12:21:18 -0400

    Spec for MenuBarCell

---
+++ spec/cells/menu_bar_cell_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe MenuBarCell do
+  context "cell rendering" do 
+    
+    context "rendering display" do
+			before :each do
+				@menubar = Menu.make! :menubar, name: 'menubar'
+				@menubar.should_not be_nil
+				@menubar.children.should_not be_nil
+			end
+				
+			it "renders the menubar as a ul" do
+				res = render_cell :menu_bar, :display, menu: @menubar
+				res.should have_selector('ul', id: 'menubar', class: 'hmenu')
+			end
+
+			it "renders each child as a Li a" do
+				@menubar.children << ( Menu.make! text: 'Home', link: '#')
+				res = render_cell :menu_bar, :display, menu: @menubar
+				res.should have_selector('ul li', id: 'home')
+				res.should have_selector('ul li a', href: '#', text: 'Home')
+			end
+			
+			it "recursively renders each child of a child as a ul" do
+				@menubar.children << ( Menu.make! text: 'Home', link: '#')
+				m2 = Menu.make! text: 'Products', link: '#'
+				@menubar.children << m2
+				m2a = Menu.make! text: 'Product 1', link: '/products/1'
+				m2.children << m2a
+				m2b = Menu.make! text: 'Product 2', link: '/products/2'
+				m2.children << m2b
+				res = render_cell :menu_bar, :display, menu: @menubar
+				
+				p = res.find('ul', id: 'products')
+				p.should_not be_nil
+				p.should have_selector('li', text: 'Product 1')
+				p.should have_selector('li', text: 'Product 2')
+			end
+				
+
+		end
+	end
+
+  context "cell instance" do 
+    subject { cell(:menu_bar) } 
+    
+    it { should respond_to(:display) }
+    
+  end
+end

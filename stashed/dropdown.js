82042f005a7f3bd93c979fbb0a1e59648c712666
Meredith Lesly <bitcababy+github@gmail.com>
2012-05-04 12:10:38 -0400

    Changed mega-menu to hmenu

--- stashed/dropdown.js
+++ stashed/dropdown.js
@@ -1,52 +1,52 @@
 (function() {
 
   $(document).ready(function($) {
-    $("#mega-menu-1").dcMegaMenu({
+    $("#hmenu-1").dcMegaMenu({
       rowItems: "3",
       speed: 0,
       effect: "slide",
       event: "click",
       fullWidth: true
     });
-    $("#mega-menu-2").dcMegaMenu({
+    $("#hmenu-2").dcMegaMenu({
       rowItems: "1",
       speed: "fast",
       effect: "fade",
       event: "click"
     });
-    $("#mega-menu-3").dcMegaMenu({
+    $("#hmenu-3").dcMegaMenu({
       rowItems: "2",
       speed: "fast",
       effect: "fade"
     });
-    $("#mega-menu-4").dcMegaMenu({
+    $("#hmenu-4").dcMegaMenu({
       rowItems: "3",
       speed: "fast",
       effect: "fade"
     });
-    $("#mega-menu-5").dcMegaMenu({
+    $("#hmenu-5").dcMegaMenu({
       rowItems: "4",
       speed: "fast",
       effect: "fade"
     });
-    $("#mega-menu-6").dcMegaMenu({
+    $("#hmenu-6").dcMegaMenu({
       rowItems: "3",
       speed: "slow",
       effect: "slide",
       event: "click",
       fullWidth: true
     });
-    $("#mega-menu-7").dcMegaMenu({
+    $("#hmenu-7").dcMegaMenu({
       rowItems: "3",
       speed: "fast",
       effect: "slide"
     });
-    $("#mega-menu-8").dcMegaMenu({
+    $("#hmenu-8").dcMegaMenu({
       rowItems: "3",
       speed: "fast",
       effect: "fade"
     });
-    return $("#mega-menu-9").dcMegaMenu({
+    return $("#hmenu-9").dcMegaMenu({
       rowItems: "3",
       speed: "fast",
       effect: "fade"
--- stashed/dropdown1.js
+++ stashed/dropdown1.js
@@ -1,7 +1,7 @@
 (function() {
 
   $(document).ready(function($) {
-    return $("#mega-menu-1").dcMegaMenu({
+    return $("#hmenu-1").dcMegaMenu({
       rowItems: "3",
       speed: 0,
       effect: "slide",
